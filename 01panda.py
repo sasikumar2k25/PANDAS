5683ca85ac69e7100b11ac9e3f87f73da5990d56
Your Name <bathalasasi25@gmail.com>
2025-02-14 00:29:58 +0530

    PANDAS

---
+++ 01panda.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
+
+print(pd.__version__)
+import pandas as pd  
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+df = pd.DataFrame(data)  # Creating a DataFrame
+print(df)
